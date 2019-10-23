1166252c561a5caeec687ae4e67db077cf9eadc6
Nathan Marley <nathan.marley@gmail.com>
2019-10-23 18:16:23 -0300

    Add BLS key generation and support JSON pretty

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/spf13/cobra v0.0.5
 	gitlab.com/dashx/dashutil v0.0.0-20190621195151-051896fef3b9
 	gitlab.com/dashx/dashx v0.0.0-20181001152421-232882fe44f0
+	gitlab.com/nmarley/go-bls-signatures v0.0.0-20191019005850-e01710ebdef9
 	gitlab.com/samli88/go-x11-hash v0.0.0-20180610202919-e5ce9e6dea1c // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
 	"gitlab.com/dashx/dashutil"
 	"gitlab.com/dashx/dashx/btcec"
 	"gitlab.com/dashx/dashx/chaincfg"
+
+	bls "gitlab.com/nmarley/go-bls-signatures"
 )
 
 // TODO: Add networks for Dash ECDSA keys (mainnet, testnet) -OR- even a flag
@@ -18,7 +22,38 @@ var cmd = &cobra.Command{
 	Use:   "genkey",
 	Short: "",
 	Long:  "",
-	Run:   cmdFunc,
+	// Run:   blsGenKeyFunc,
+	Run: cmdFunc,
+}
+
+// KeyPair represents an asynchronous cryptographic key pair
+type KeyPair struct {
+	SecretKey string `json:"secret"`
+	PublicKey string `json:"public"`
+}
+
+// String implements the Stringer interface
+func (kp KeyPair) String() string {
+	jsonRepr, err := json.MarshalIndent(kp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return string(jsonRepr)
+}
+
+func blsGenKeyFunc(c *cobra.Command, inp []string) {
+	sk, err := bls.RandKey(rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	pk := sk.PublicKey()
+
+	keypair := KeyPair{
+		SecretKey: fmt.Sprintf("%64x", sk.Serialize()),
+		PublicKey: fmt.Sprintf("%96x", pk.Serialize()),
+	}
+	fmt.Println(keypair)
 }
 
 func cmdFunc(c *cobra.Command, inp []string) {
@@ -27,14 +62,24 @@ func cmdFunc(c *cobra.Command, inp []string) {
 		panic(err)
 	}
 
-	// wif, err := dashutil.NewWIF(sk, &chaincfg.TestNet3Params, true)
-	wif, err := dashutil.NewWIF(sk, &chaincfg.MainNetParams, true)
+	var network *chaincfg.Params
+	network = &chaincfg.MainNetParams
+	// network = &chaincfg.TestNet3Params
+	wif, err := dashutil.NewWIF(sk, network, true)
 	if err != nil {
 		panic(err)
 	}
 
-	//fmt.Printf("sk bytes = %64x\n", bytes)
-	fmt.Println(wif.String())
+	addr, err := dashutil.NewAddressPubKey(wif.SerializePubKey(), network)
+	if err != nil {
+		panic(err)
+	}
+
+	keypair := KeyPair{
+		SecretKey: wif.String(),
+		PublicKey: addr.EncodeAddress(),
+	}
+	fmt.Println(keypair)
 }
 
 func main() {
